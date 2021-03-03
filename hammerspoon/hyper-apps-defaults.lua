0736054f711d750b2e87981ff20047ae97f47971
Fabio M. Costa <fabiomcosta@gmail.com>
2021-03-03 14:48:11 -0800

    Some customizations for my keyboard setup

--- hammerspoon/hyper-apps-defaults.lua
+++ hammerspoon/hyper-apps-defaults.lua
@@ -4,13 +4,11 @@
 -- this file, save it as `hyper-apps.lua`, and edit the table below to configure
 -- your preferred shortcuts.
 return {
-  { 'a', 'Music' },             -- "A" for "Apple Music"
-  { 'b', 'Google Chrome' },     -- "B" for "Browser"
-  { 'c', 'Slack' },             -- "C for "Chat"
-  { 'd', 'Remember The Milk' }, -- "D" for "Do!" ... or "Done!"
-  { 'e', 'Atom' },              -- "E" for "Editor"
-  { 'f', 'Finder' },            -- "F" for "Finder"
-  { 'g', 'Mailplane 3' },       -- "G" for "Gmail"
-  { 's', 'Slack' },             -- "S" for "Slack"
-  { 't', 'iTerm' },             -- "T" for "Terminal"
+  { 'm', 'Spotify' },
+  { 'b', 'Google Chrome' },
+  { 'c', 'Slack' },
+  { 'f', 'Finder' },
+  { 'e', 'Microsoft Outlook' },
+  { 't', 'Alacritty' },
+  { 'n', 'Notion' },
 }
