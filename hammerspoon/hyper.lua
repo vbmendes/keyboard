52a9597dee3b1bf089137c1bdef9e7076900af01
Fabio M. Costa <fabiomcosta@gmail.com>
2021-12-11 17:28:14 -0800

    Allowing cycling through windows from the same application

--- hammerspoon/hyper.lua
+++ hammerspoon/hyper.lua
@@ -4,12 +4,58 @@ if not status then
   hyperModeAppMappings = require('keyboard.hyper-apps-defaults')
 end
 
-for i, mapping in ipairs(hyperModeAppMappings) do
+local function tableLength(t)
+  local count = 0
+  for _ in pairs(t) do count = count + 1 end
+  return count
+end
+
+local function tableFindIndex(t, fn)
+  for i, item in ipairs(t) do
+    if (fn(item, i)) then
+      return i
+    end
+  end
+  return nil
+end
+
+for _, mapping in ipairs(hyperModeAppMappings) do
   local key = mapping[1]
   local app = mapping[2]
+
+  -- We have to keep a reference to the table containing all windows because
+  -- the table returned by application:allWindows() keeps changing its order.
+  -- I think it returns an array ordered by their z-index.
+  -- By keeping the original order we are able to properly cycle through the
+  -- available windows.
+  local allWindows = nil
+
   hs.hotkey.bind({'shift', 'ctrl', 'alt', 'cmd'}, key, function()
     if (type(app) == 'string') then
-      hs.application.open(app)
+      local application = hs.application.get(app)
+
+      local newWindows = application:allWindows()
+
+      -- First time we try to either open or focus on this application
+      if application == nil or not application:isFrontmost() then
+        hs.application.open(app)
+        allWindows = newWindows
+        return
+      end
+
+      -- If for some reason allWindows is not properly set on the previous call
+      if allWindows == nil then
+        allWindows = newWindows
+      end
+
+      local windowsLength = tableLength(allWindows)
+      if (windowsLength > 1) then
+        local focusedWindow = application:focusedWindow()
+        local focusedIndex = tableFindIndex(allWindows, function(window) return window == focusedWindow end)
+        local nextIndex = (focusedIndex % windowsLength) + 1
+        allWindows[nextIndex]:focus()
+      end
+
     elseif (type(app) == 'function') then
       app()
     else
